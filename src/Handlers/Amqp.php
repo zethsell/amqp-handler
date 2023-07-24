dffcf626bd7c20b5f5e4823a04ef1223bb95110e
Rodrigo Carreirão <rodrigo.carreirao@gmail.com>
2023-07-24 20:30:42 -0300

    Adjusting settings.

--- src/Handlers/Amqp.php
+++ src/Handlers/Amqp.php
@@ -31,7 +31,7 @@ class Amqp
                 $password,
                 '/',
                 ['verify_peer' => true],
-                ['read_write_timeout' => 60, 'heartbeat' => 30],
+                ['read_write_timeout' => 360, 'heartbeat' => 40],
                 'ssl',
                 $connectionConfig
             );
